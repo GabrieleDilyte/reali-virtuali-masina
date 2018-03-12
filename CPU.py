243f856c22799f440a0e1aa5898116b22f107b64
Masilionytė Dalia <dmasilionyte@gmail.com>
2018-03-12 19:09:14 +0200

    rewrite with python

---
+++ CPU.py
@@ -0,0 +1,82 @@
+class CPU:
+	USER = 0
+	SUPERVISOR = 1
+	
+	MODE = SUPERVISOR
+
+	# Registrai tik realiai
+	TI = 40
+
+	# Ar galime PLR tureti 1 baito?
+	PLR = 1
+
+	# Ir virtualios registrai
+
+	IC = [0, 0]
+	SF = 0
+	R = [0, 0, 0, 0] 
+# Kokia turetu buti stack pointerio reiksme
+	SP = [0, 0]
+
+
+	def reset_CPU():
+
+		TI = 40
+
+
+	def ADD():
+
+
+	def SUB():
+
+
+	def MUL();
+
+
+	def DIV():
+
+
+	def CMPR():
+
+
+	def PRINT():
+
+
+	def PRINTI():
+
+
+	def READ():
+
+
+
+	def READi():
+
+
+	def SETR():
+
+
+	def PULLR():
+
+
+
+	def JMP():
+
+
+	def HALT():
+
+
+	def INT():
+
+
+
+
+
+
+
+
+
+
+
+
+
+
